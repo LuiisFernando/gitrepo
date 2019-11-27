65f878ee7db1615af72fe836a3f449503e05cca4
Luis Fernando <lsh.fernandors@gmail.com>
2019-11-26 21:18:00 -0300

    deploy files

---
+++ docs/precache-manifest.01ceb3351c7dcb1b1252dff3f05371bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6a9d877cc6e27dc9f900400b364fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b162cf1a09dc0827a9dc",
+    "url": "/static/js/2.8bf87f6d.chunk.js"
+  },
+  {
+    "revision": "5af6d53f37df0617955f",
+    "url": "/static/js/main.b07fd95b.chunk.js"
+  },
+  {
+    "revision": "43f84a4041739e45bbaf",
+    "url": "/static/js/runtime~main.5a6afcb1.js"
+  }
+]);
