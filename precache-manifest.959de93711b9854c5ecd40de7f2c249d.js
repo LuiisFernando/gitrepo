701735a47a04a86d6ed2098cfa48800fc06b205d
Luis Fernando <lsh.fernandors@gmail.com>
2019-11-27 01:19:22 -0300

    Updates

---
+++ precache-manifest.959de93711b9854c5ecd40de7f2c249d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7661d30bc8c08edcf5e375396a5c00",
+    "url": "/gitrepo/index.html"
+  },
+  {
+    "revision": "c12dffe02b7f7a3e9449",
+    "url": "/gitrepo/static/js/2.f372c966.chunk.js"
+  },
+  {
+    "revision": "5af6d53f37df0617955f",
+    "url": "/gitrepo/static/js/main.b07fd95b.chunk.js"
+  },
+  {
+    "revision": "b22dfc3b6838f48d47ea",
+    "url": "/gitrepo/static/js/runtime~main.d48f2f76.js"
+  }
+]);
